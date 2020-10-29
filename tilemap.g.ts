ab99b4828a8296649c01487d877a3f5df8b1e74c
Shannon Kao <shakao@microsoft.com>
2020-10-28 17:54:53 -0700

    Remove tilemap jres files

--- tilemap.g.ts
+++
@@ -1,7 +0,0 @@
-// Auto-generated code. Do not edit.
-namespace myTiles {
-    //% fixedInstance jres blockIdentity=images._tile
-    export const transparency16 = image.ofBuffer(hex``);
-
-}
-// Auto-generated code. Do not edit.
